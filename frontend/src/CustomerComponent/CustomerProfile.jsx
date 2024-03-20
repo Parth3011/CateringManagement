c4bb4a56d6dfbd895ac3e5eaa7806b7d03a14b1c
Parth3011 <parthchauhan348@gmail.com>
2024-03-20 10:04:09 +0530

    customer Profile

--- frontend/src/CustomerComponent/CustomerProfile.jsx
+++ frontend/src/CustomerComponent/CustomerProfile.jsx
@@ -1,78 +1,124 @@
-import React, {useState} from 'react'
+import React, { useState} from 'react'
 
 import '../Css/signup.css'
+import axios from 'axios';
 
 export default function CustomerProfile({user}) {
       console.log(user);
-    const[uname,setUname] = useState("");
-    const[email,setEmail] = useState("");
-//     const[company,setCompany] = useState("");
-    const[pwd,setPwd] = useState("");
-    const[phone,setPhone] = useState("");
-    const[address,setAddress] = useState("");
-    const[pin,setPin] = useState("");
-    const[city,setCity] = useState("");
-    const[state,setState] = useState("");
-
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-    };
+      const [inputdata, setinputData] = useState(user);
+     
+        
+          const handledata = (e) => {
+            setinputData({ ...inputdata, [e.target.name]: e.target.value || "" });
+          };
+        
+          const handleSubmit = (event) => {
+            event.preventDefault();
+            console.log(inputdata);
+            // confirm("Confirm to update your profile");
+        
+              try {
+                axios
+                  .put("http://localhost:7000/api/updateprofile", inputdata)
+                  .then((resp) => {
+                    alert("Data Updated Successfully")
+                  });
+                
+              } catch (error) {
+                console.log(error);
+              }
+          };
 
     return (
-    <div className='registration'>
-    <h1>Profile</h1>
-    <div className='box'>
-      <label>Username</label>
-      <input type = "text" className='uname' name="uname" 
-                value={uname} onChange={(e)=>{setUname(e.target.value)}} 
-      /><br/>
-
-
-      <label>Email</label>
-      <input type = "text" className='email' name="email" 
-             value={email} onChange={(e)=>{setEmail(e.target.value)}}
-      /><br/>
-      
-      {/* <label>Company Name</label>
-      <input type = "text" className='company' name="company" 
-            value={company} onChange={(e)=>{setCompany(e.target.value)}}
-      /><br/> */}
-
-      <label>Password</label>
-      <input type = "password" className='pwd' name="pwd" 
-            value={pwd} onChange={(e)=>{setPwd(e.target.value)}} 
-      /><br/>
-
-
-      <label>Phone No.</label>
-      <input type = "number" className='phone' name="phone" 
-                value={phone} onChange={(e)=>{setPhone(e.target.value)}} 
-      /><br/>
-
-      <label>Address</label>
-      <textarea className='address' rows="1" name="address" 
-             value={address} onChange={(e)=>{setAddress(e.target.value)}}
-      ></textarea><br/>
-
-      <label>Pincode</label>
-      <input type = "number" className='pin' name="pin" 
-                value={pin} onChange={(e)=>{setPin(e.target.value)}} 
-      /><br/> 
-
-      <label>City</label>
-      <input type = "text" className='city' name="city" 
-            value={city} onChange={(e)=>{setCity(e.target.value)}} 
-      /><br/>
-
-      <label>State</label>
-      <input type = "text" className='state' name="state" 
-            value={state} onChange={(e)=>{setState(e.target.value)}} 
-      /><br/>
-
-      <input type="submit" className="register bg-success" value="Update" onClick={handleSubmit} />
-
-    </div>
-    </div>
-  )
-}
+      <div className="registration">
+        <h1>Profile</h1>
+        <div className="box">
+          <label>Username</label>
+          <input
+            type="text"
+            className="name"
+            name="name"
+            value={inputdata.name}
+            onChange={handledata}
+          />
+          <br />
+  
+          <label>Email</label>
+          <input
+            type="text"
+            className="email"
+            name="email"
+            value={inputdata.email}
+            onChange={handledata}
+            disabled
+          />
+          <br />
+  
+  
+          {/* <label>Password</label>
+        <input type = "text" className='pwd' name="pwd" 
+              value={pwd} onChange={(e)=>{setPwd(e.target.value)}} 
+              disabled
+        /><br/> */}
+  
+          {/* <label>Confirm Password</label>
+        <input type = "password" className='confirm' name="confirm" 
+              value={confirm} onChange={(e)=>{setConfirm(e.target.value)}} 
+        /><br/> */}
+  
+          <label>Phone No.</label>
+          <input
+            type="number"
+            className="phone"
+            name="phone"
+            value={inputdata.phone}
+            onChange={handledata}
+          />
+          <br />
+  
+          <label>Address</label>
+          <textarea
+            className="address"
+            rows="1"
+            name="address"
+            value={inputdata.address}
+            onChange={handledata}
+          ></textarea>
+          <br />
+  
+          <label>Pincode</label>
+        <input type = "number" className='pin' name="pincode" 
+                  value={inputdata.pincode} onChange={handledata} 
+        /><br/>
+  
+          <label>City</label>
+          <input
+            type="text"
+            className="city"
+            name="city"
+            value={inputdata.city}
+            onChange={handledata}
+          />
+          <br />
+  
+          <label>State</label>
+          <input
+            type="text"
+            className="state"
+            name="state"
+            value={inputdata.state}
+            onChange={handledata}
+          />
+          <br />
+  
+          <input
+            type="submit"
+            className="register bg-success"
+            value="Update"
+            onClick={handleSubmit}
+          />
+        </div>
+      </div>
+    );
+  }
 
